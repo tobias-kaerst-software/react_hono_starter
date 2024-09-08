ae8985eadb6daea3c701d600e19e73864cf8573c
Tobias Kärst <github@tobiaskaerst.dev>
2024-09-08 13:56:20 +0200

    feat: adjusted switch style

--- frontend/src/components/ui/switch.tsx
+++ frontend/src/components/ui/switch.tsx
@@ -1,26 +1,39 @@
 import { Root, Thumb } from '@radix-ui/react-switch';
+import { cva, VariantProps } from 'class-variance-authority';
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 
 import { cn } from '$/lib/utils';
 
-const Switch = forwardRef<ElementRef<typeof Root>, ComponentPropsWithoutRef<typeof Root>>(function Switch(
-  { className, ...props },
+const rootVariants = cva(
+  'peer inline-flex shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+  {
+    defaultVariants: { size: 'default' },
+    variants: { size: { default: 'h-8 w-16', sm: 'h-6 w-12' } },
+  },
+);
+
+const thumbVariants = cva(
+  'pointer-events-none block rounded-full bg-background shadow-lg ring-0 transition-transform',
+  {
+    defaultVariants: { size: 'default' },
+    variants: {
+      size: {
+        default: 'size-6 data-[state=checked]:translate-x-[32px] data-[state=unchecked]:translate-x-1',
+        sm: 'size-[18px] data-[state=checked]:translate-x-[23px] data-[state=unchecked]:translate-x-[2px]',
+      },
+    },
+  },
+);
+
+export type SwitchProps = ComponentPropsWithoutRef<typeof Root> & VariantProps<typeof rootVariants>;
+
+const Switch = forwardRef<ElementRef<typeof Root>, SwitchProps>(function Switch(
+  { className, size = 'default', ...props },
   ref,
 ) {
   return (
-    <Root
-      className={cn(
-        'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
-        className,
-      )}
-      {...props}
-      ref={ref}
-    >
-      <Thumb
-        className={cn(
-          'pointer-events-none block size-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0',
-        )}
-      />
+    <Root className={cn(rootVariants({ className, size }))} {...props} ref={ref}>
+      <Thumb className={cn(thumbVariants({ size }))} />
     </Root>
   );
 });
